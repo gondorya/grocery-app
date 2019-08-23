ee4ac69f20fb3bb11709441de68b06780bbe7c57
Monika Filipowska <>
2019-08-23 20:57:57 +0200

    single recipe

--- src/components/Recipe.tsx
+++ src/components/Recipe.tsx
@@ -1,11 +1,23 @@
 import React from 'react';
+import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 
 const Recipe = (props) => (
 	<div>
-		<h2>Post {props.match.params.id}</h2>
+		<h2>Post {props.recipe.title}</h2>
+		<p>published at {props.recipe.date}</p>
 		<Link to={`/recipes/edit/${props.match.params.id}`}>Edit</Link>
+		<p>{props.recipe.description}</p>
+		<img src={props.recipe.img} alt={props.recipe.title} />
+		<ul>{props.recipe.ingredients.map((ingredient) => <li>{ingredient}</li>)}</ul>
+		<ol>{props.recipe.steps.map((step) => <li>{step}</li>)}</ol>
 	</div>
 );
 
-export default Recipe;
+const mapStateToProps = (state, props) => {
+	return {
+		recipe: state.recipes.find((recipe) => recipe.id === props.match.params.id)
+	};
+};
+
+export default connect(mapStateToProps)(Recipe);
